e7d4fced4c856d32544ded3a2f3feedd05535932
Stephen Nimmo <stephennimmo@gmail.com>
2021-02-10 11:38:46 -0600

    test github actions

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
+    title: 'Easy to Use2',
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
