a3029d9235b9b9295f07c99f771ca8e7b696bb3a
Stephen Nimmo <stephennimmo@gmail.com>
2021-02-10 09:19:03 -0600

    updated logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ module.exports = {
   projectName: 'redhat-energy-pod.github.io', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Red Hat Energy Pod',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
